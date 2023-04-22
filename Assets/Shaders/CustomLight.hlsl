644376ce11702bbbbf95146d715eb5aeda5cdcd1
EricHu <zidane0524@gmail.com>
2023-04-22 20:52:39 +0800

    update water shader to use phase function for calculating shallow surface color

--- Assets/Shaders/CustomLight.hlsl
+++ Assets/Shaders/CustomLight.hlsl
@@ -11,3 +11,18 @@ void MainLight_float(out float3 Direction, out float3 Color, out float DistanceA
         DistanceAtten = mainLight.distanceAttenuation;
     #endif
 }
+
+void HenyeyGreensteinPhase_float(float lightDotView, float g, out float Out)
+{
+    Out = 0;
+    float denom = 1.0 + g * g + 2.0 * g * lightDotView;
+    Out = (1.0 - g * g) / (denom * sqrt(denom));
+}
+
+void SchlickPhase_float(float dotNH, float refractionIndex, out float Out)
+{
+    Out = 0;
+    float r0 = pow((1.0f - refractionIndex) / (1.0f + refractionIndex), 2.0f);
+    float c = 1.0f - abs(dotNH);
+    Out = r0 + (1.0f - r0) * pow(c, 5.0f);
+}
--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -6,15 +6,25 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+    public TMPro.TextMeshProUGUI TileValue;
     public TMPro.TextMeshProUGUI DisplacementValue;
     public TMPro.TextMeshProUGUI NormalStrengthValue;
     public TMPro.TextMeshProUGUI FramerateValue;
     public Material Mat;
     public Transform Sun;
 
+    private int originTileValue;
+
     private float originDisplacementValue;
     private float originNormalStrengthValue;
     private int originFramerateValue;
+
+    public void OnTileChange(float value)
+    {
+        var intValue = (int)value;
+        Mat.SetFloat("_Tile", (float)intValue);
+        TileValue.text = "" + intValue;
+    }
 
 
     public void OnDisplacementChange(float value)
@@ -48,17 +58,24 @@ public class UIController : MonoBehaviour
         originDisplacementValue = Mat.GetFloat("_Displacement");
         originNormalStrengthValue = Mat.GetFloat("_NormalStrength");
         originFramerateValue = Mat.GetInt("_FrameRate");
+        originTileValue = (int)Mat.GetFloat("_Tile");
+
+        TileValue.text = "" + (int)Mat.GetFloat("_Tile");
+        TileValue.GetComponentInParent<Slider>().value = originTileValue;
 
         DisplacementValue.text = "" + Mat.GetFloat("_Displacement");
         DisplacementValue.GetComponentInParent<Slider>().value = originDisplacementValue;
+
         NormalStrengthValue.text = "" + Mat.GetFloat("_NormalStrength");
         NormalStrengthValue.GetComponentInParent<Slider>().value = originNormalStrengthValue;
+
         FramerateValue.text = "" + Mat.GetInt("_FrameRate");
         FramerateValue.GetComponentInParent<Slider>().value = originFramerateValue;
     }
 
     private void OnDestroy()
     {
+        Mat.SetFloat("_Tile", originTileValue);
         Mat.SetFloat("_Displacement", originDisplacementValue);
         Mat.SetFloat("_NormalStrength", originNormalStrengthValue);
         Mat.SetInt("_FrameRate", (int)originFramerateValue);
